4b50ef918bf62c28167469b17c965300fa73226e
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-17 23:02:20 +0530

    initial commit

---
+++ pages/PlayPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>Play Page</div>
+    <NuxtLink to="/"> Home Page </NuxtLink>
+  </div>
+</template>
+
+<script>
+console.log($nuxt)
+
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
